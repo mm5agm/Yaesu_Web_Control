1647790ff72248064e82cc0883e89bf36b9a7dc4
mm5agm <mm5agm@outlook.com>
2026-09-15 14:31:42 +0100

    Release: pending changes for v2.5.1

--- Models/AppVersion.cs
+++ Models/AppVersion.cs
@@ -2,11 +2,11 @@ namespace Yaesu_Web_Control;
 
 public static class AppVersion
 {
-    public const string Current = "2.5.0";
+    public const string Current = "2.5.1";
 
     /// <summary>Date this version was released, ISO format.
     /// Bump on actual release; current value reflects the planned ship date.</summary>
-    public const string ReleaseDate = "2026-09-14";
+    public const string ReleaseDate = "2026-09-15";
 
     /// <summary>
     /// Firmware versions of the developer's bench radio(s) at the time this
