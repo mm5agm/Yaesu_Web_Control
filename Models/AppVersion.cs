6d89129f229a143c98abb25c1832511989444bda
mm5agm <mm5agm@outlook.com>
2026-09-15 16:05:03 +0100

    Pre-release: pending changes for v2.5.2-pre1

--- Models/AppVersion.cs
+++ Models/AppVersion.cs
@@ -2,7 +2,7 @@ namespace Yaesu_Web_Control;
 
 public static class AppVersion
 {
-    public const string Current = "2.5.1";
+    public const string Current = "2.5.2";
 
     /// <summary>Date this version was released, ISO format.
     /// Bump on actual release; current value reflects the planned ship date.</summary>
